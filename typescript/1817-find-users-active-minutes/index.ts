b74b00c76db3acad47162e8941cfd1b66807b94d
samcurteis <samcurteis@gmail.com>
2023-08-21 11:30:52 +0100

    Edited 1817 to function efficiently at scale of user ids

--- typescript/1817-find-users-active-minutes/index.ts
+++ typescript/1817-find-users-active-minutes/index.ts
@@ -3,32 +3,31 @@ export default function findingUsersActiveMinutes(
   k: number
 ): number[] {
   const hash = new Map();
-  const counter = [];
   const response = [];
 
+  // iterate through the logs
+  // if hash doesn't contain id, set id as key, minute as value, increment counter for array at index of id
+  // if hash doesn't already contain minute in the key, increment counter
   logs.forEach((log) => {
     const id = log[0];
-    const number = log[1];
-    if (counter[id] === undefined) {
-      counter[id] = 0;
-    }
-    if (!hash.has(id)) {
-      hash.set(id, [number]);
-      counter[id] += 1;
-    } else if (!hash.get(id).includes(number)) {
-      hash.set(id, [...hash.get(id), number]);
-      counter[id] += 1;
+    const minute = log[1];
+    const hashId = hash.get(id);
+
+    if (!hashId) {
+      hash.set(id, [[minute], 1]);
+    } else if (!hashId[0].includes(minute)) {
+      hash.set(id, [[...hashId[0], minute], hashId[1] + 1]);
     }
   });
 
+  // from 1 to k, create an array of zeros
   for (let i = 0; i < k; i++) {
     response[i] = 0;
   }
 
-  for (let i = 0; i < counter.length; i++) {
-    if (counter[i] !== undefined) {
-      response[counter[i] - 1] += 1;
-    }
+  // iterate over array, if contains value, increment corresponding index by 1
+  for (let value of hash.values()) {
+    response[value[1] - 1] += 1;
   }
 
   return response;
